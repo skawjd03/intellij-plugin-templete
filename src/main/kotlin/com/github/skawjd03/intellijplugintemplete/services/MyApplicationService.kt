9c41c9d9e243216be075e42664388c506a6aebb7
GitHub Action <action@github.com>
2022-12-27 07:14:07 +0000

    Template cleanup

--- src/main/kotlin/com/github/skawjd03/intellijplugintemplete/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/skawjd03/intellijplugintemplete/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.skawjd03.intellijplugintemplete.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.skawjd03.intellijplugintemplete.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/skawjd03/intellijplugintemplete/services/MyProjectService.kt
+++ src/main/kotlin/com/github/skawjd03/intellijplugintemplete/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.skawjd03.intellijplugintemplete.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.skawjd03.intellijplugintemplete.MyBundle
 
 class MyProjectService(project: Project) {
 
